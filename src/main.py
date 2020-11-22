01bec3afec1d71286645b7f11473097098fbc830
Thomas Lämmlein <thomaslaemmlein@gmail.com>
2020-11-22 16:43:29 +0100

    Raketen verschwinden aus dem Spielfeld oder treffen Aliens.

--- src/main.py
+++ src/main.py
@@ -20,28 +20,40 @@ rockets=[]
 for x in range(anzahl_aliens):
     alien = Actor("alien")
     alien.pos = (30+alien_abstand*x),30
+    alien.status = 0
     aliens.append(alien)
 
-def bewege_rockets():
-    print("bewege " + str(len(rockets)) + " rockets")
+def update_rockets():
+    global rockets, score, aliens
     for r in rockets:
         r.y = r.y - 5
         if (r.y <0):
-            rockets.remove(r)
-        
-            
-    clock.schedule(bewege_rockets, 0.01)
+            r.status=1
+            continue
+        for a in aliens:
+            rocket_shoot_alien = r.colliderect(a)
+            if rocket_shoot_alien:
+                r.status=1
+                a.status = 1
+                score =score + 1
+    rockets = listCleanup(rockets)
+    aliens = listCleanup(aliens)
+    #print("Anzahl rockets: " + str(len(rockets)))
 
-clock.schedule(bewege_rockets, 0.01)
+def listCleanup(l):
+    newList = []
+    for i in range(len(l)):
+        if l[i].status == 0: newList.append(l[i])
+    return newList
 
 # Ende globale initializierung
 
 def draw():
-    global score
+    global score, aliens
     screen.clear()
     ship.draw()
-    for x in range(anzahl_aliens):
-        aliens[x].draw()
+    for a in aliens:
+        a.draw()
     screen.draw.text("Punkte: " + str(score), color="yellow", topleft=(5, 1))
     
     for r in rockets:
@@ -59,16 +71,13 @@ def update():
         ship.x = ship.x + 4
         if (ship.x > WIDTH):
             ship.x = WIDTH
-    for r in rockets:
-        for a in aliens:
-            rocket_shoot_alien = r.colliderect(a)
-            if rocket_shoot_alien:
-                score =score + 1
+    update_rockets()            
 
 def on_key_up(key):
     if key == keys.SPACE:
         rocket = Actor("rocket")
         rocket.pos = ship.x, ship.y -25
+        rocket.status = 0
         rockets.append(rocket)
         print("Feuer")
 
